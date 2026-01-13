fec73356297c5a6783b33c18b648be9bda1fd76d
mateusdornellascf <mateus.freitas@serpro.gov.br>
2026-01-13 16:49:31 -0300

    Primeiro commit teste

---
+++ projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/Startup.java
@@ -0,0 +1,13 @@
+package br.com.mateusdornellascf.projeto_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
