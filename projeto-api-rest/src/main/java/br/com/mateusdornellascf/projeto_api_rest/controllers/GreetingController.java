f8409e0cc8f14ca5e2edb6b694d12ae1ca726ba1
mateusdornellascf <mateus.freitas@serpro.gov.br>
2026-01-14 12:01:59 -0300

    Primeiro Endpoint Rest

---
+++ projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/controllers/GreetingController.java
@@ -0,0 +1,26 @@
+package br.com.mateusdornellascf.projeto_api_rest.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mateusdornellascf.projeto_api_rest.models.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+    
+    //localhost:8080/greeting?name=xxxxx
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+}
---
+++ projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/models/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.mateusdornellascf.projeto_api_rest.models;
+
+public record Greeting(long id, String content) {
+    
+}
