05896459daa5243afde9e291c7791a69c335690a
mateusdornellascf <mateus.freitas@serpro.gov.br>
2026-01-15 13:30:53 -0300

    Math Controller

---
+++ projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/controllers/MathController.java
@@ -0,0 +1,39 @@
+package br.com.mateusdornellascf.projeto_api_rest.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+    // Path Param
+    // localhost:8080/greeting/xx
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException{
+        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+         
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty())
+            return false;
+
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+}
