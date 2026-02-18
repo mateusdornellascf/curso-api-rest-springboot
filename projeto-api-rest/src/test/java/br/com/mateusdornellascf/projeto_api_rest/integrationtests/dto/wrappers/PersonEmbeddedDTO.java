3468cf929c0cecfc37520fe06f1e16b97c3e6d38
mateusdornellascf <mateus.dornellascf@gmail.com>
2026-02-18 11:27:09 -0300

    feat: adiciona novo teste PersonControllerJson

---
+++ projeto-api-rest/src/test/java/br/com/mateusdornellascf/projeto_api_rest/integrationtests/dto/wrappers/PersonEmbeddedDTO.java
@@ -0,0 +1,31 @@
+package br.com.mateusdornellascf.projeto_api_rest.integrationtests.dto.wrappers;
+
+import br.com.mateusdornellascf.projeto_api_rest.integrationtests.dto.PersonDTO;
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PersonEmbeddedDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("people")
+    private List<PersonDTO> people;
+
+    public PersonEmbeddedDTO() {
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public List<PersonDTO> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<PersonDTO> people) {
+        this.people = people;
+    }
+
+}
---
+++ projeto-api-rest/src/test/java/br/com/mateusdornellascf/projeto_api_rest/integrationtests/dto/wrappers/WrapperPersonDTO.java
@@ -0,0 +1,24 @@
+package br.com.mateusdornellascf.projeto_api_rest.integrationtests.dto.wrappers;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WrapperPersonDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @JsonProperty("_embedded")
+    private PersonEmbeddedDTO embedded;
+
+    public WrapperPersonDTO(){}
+
+    public PersonEmbeddedDTO getEmbedded() {
+        return embedded;
+    }
+
+    public void setEmbedded(PersonEmbeddedDTO embedded) {
+        this.embedded = embedded;
+    }
+    
+}
