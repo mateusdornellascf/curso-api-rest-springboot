83099cff8bbf80d2017aeef5985879b93946721e
mateusdornellascf <mateus.dornellascf@gmail.com>
2026-02-22 10:52:41 -0300

    feat: cria o arquivo responsavel por armazenar o arquivo no disco

---
+++ projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/services/v3/FileStorageService.java
@@ -0,0 +1,51 @@
+package br.com.mateusdornellascf.projeto_api_rest.services.v3;
+
+
+import br.com.mateusdornellascf.projeto_api_rest.config.FileStorageConfig;
+import br.com.mateusdornellascf.projeto_api_rest.exceptions.FileStorageException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath()
+                .toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where files will be stored!", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename Contains a Invalid path Sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try Again!", e);
+        }
+    }
+}
