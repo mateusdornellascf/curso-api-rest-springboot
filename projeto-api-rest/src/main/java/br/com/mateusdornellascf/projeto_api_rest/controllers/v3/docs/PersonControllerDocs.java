516d27bde95f30b2b9474bfa0624664d5da5e501
mateusdornellascf <mateus.dornellascf@gmail.com>
2026-03-02 14:59:09 -0300

    feat: documenta novo endpoint no swagger

--- projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/controllers/v3/docs/PersonControllerDocs.java
+++ projeto-api-rest/src/main/java/br/com/mateusdornellascf/projeto_api_rest/controllers/v3/docs/PersonControllerDocs.java
@@ -2,6 +2,7 @@ package br.com.mateusdornellascf.projeto_api_rest.controllers.v3.docs;
 
 import java.util.List;
 
+import org.springframework.core.io.Resource;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.mateusdornellascf.projeto_api_rest.data.dto.v3.PersonDTO;
+import br.com.mateusdornellascf.projeto_api_rest.file.exporter.MediaTypes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface PersonControllerDocs {
 
@@ -56,6 +59,32 @@ public interface PersonControllerDocs {
                         @RequestParam(value = "direction", defaultValue = "asc") String direction
 
         );
+
+        @Operation(summary = "Export People",
+            description = "Export a Page of People in XLSX and CSV format",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = {
+                                @Content(mediaType = MediaTypes.APPLICATION_XLSX_VALUE),
+                                @Content(mediaType = MediaTypes.APPLICATION_CSV_VALUE)
+                            }),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<Resource> exportPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction,
+            HttpServletRequest request
+    );
+    
 
         @Operation(summary = "Massive People Creation", description = "Massive People Creation with upload of XLSX or CSV", tags = {
                         "People" }, responses = {
